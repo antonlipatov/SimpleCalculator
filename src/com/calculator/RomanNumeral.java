20500d12715a17e337cc755f8552affb6781ba35
Anton Lipatov <antonlipatov@outlook.com>
2022-04-15 10:24:16 -0700

    Added enum RomanNumeral
    Перечисление римских цифр

---
+++ src/com/calculator/RomanNumeral.java
@@ -0,0 +1,34 @@
+package com.calculator;
+
+public enum RomanNumeral {
+    I("I", 1),
+    II("II", 2),
+    III("III", 3),
+    IV("IV", 4),
+    V("V", 5),
+    VI("VI", 6),
+    VII("VII", 7),
+    VIII("VIII", 8),
+    IX("IX", 9),
+    X("X", 10);
+
+    private int value;
+    private String key;
+
+    RomanNumeral(String key, int value) {
+        this.value = value;
+        this.key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    static int romanToInt(String key){
+        return RomanNumeral.valueOf(key).value;
+    }
+}
