b2ec24e05da11453cd27a1d9ca57de3e072f421d
Anton Lipatov <antonlipatov@outlook.com>
2022-04-15 10:17:27 -0700

    main function

--- src/com/calculator/Main.java
+++ src/com/calculator/Main.java
@@ -1,8 +1,21 @@
 package com.calculator;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Введите два числа от 1 до 10 илиримские цыфры от I до X и арифмитическую операцию.");
+        System.out.println("Пример: 2 + 5, III + VI");
+        Scanner inputValue = new Scanner(System.in);
+        String requestString = inputValue.nextLine();
+        inputValue.close();
+        String responseString = new String();
+        try {
+            responseString = calc(requestString);
+        } catch (CalculatorException e) {
+            responseString = "Ошибка: "+ e.getNumber() +" " + e.getMessage();
+        }
+        System.out.println(responseString);
     }
 }
